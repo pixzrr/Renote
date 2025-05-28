8bca0d1bd7bab770c3e9ddd5e469f04630a55614
pixzrr <91895158+pixzrr@users.noreply.github.com>
2025-05-29 00:00:09 +0200

    Add files via upload

---
+++ Renote/index.php
@@ -0,0 +1,29 @@
+<?php define('ROOT', '/Renote/'); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Renote</title>
+        <meta name="description" content="">
+        <link rel="shortcut icon" href="<?= ROOT ?>images/tv.png">
+        <meta charset="utf-8">
+        <meta name="author" content="Renote">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?= ROOT ?>css/style.css">
+    </head>
+    <body>
+        <header>
+            <!--Logo-->
+            <a href="<?= ROOT ?>index.php"><h1>R</h1></a>
+
+            <!--Outils-->
+            <p class="toolbar">Mon espace</p>
+            <p class="toolbar">Editer</p>
+            <p class="toolbar">Outils</p>
+            <p class="toolbar">Affichage</p>
+            <p class="toolbar">Partager</p>
+        </header>
+        <nav>
+        </nav>
+    </body>
+</html>
